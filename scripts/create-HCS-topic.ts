7c6d94982553d6924a228c7585228d8cabc568fe
HY Lim <124254825+hyLim6519@users.noreply.github.com>
2023-09-04 14:08:40 +0800

    feat: optimize regex with better syntax and amend error handling based on best practice

--- scripts/create-HCS-topic.ts
+++ scripts/create-HCS-topic.ts
@@ -16,7 +16,7 @@ async function checkTopicId() {
 
     try{
         if (!process.env.OPERATOR_ID || !process.env.OPERATOR_KEY) {
-            throw new Error('OPERATOR_ID and OPERATOR_KEY must be set in .env.local');
+            throw new Error('OPERATOR_ID and OPERATOR_KEY must be set in your .env.local file');
         }
 
         // Create a new client instance
@@ -27,7 +27,7 @@ async function checkTopicId() {
 
         // Check if the topic ID is valid
         if (topicId) {
-            const topicIdQuery = new TopicInfoQuery({topicId: topicId});
+            const topicIdQuery = new TopicInfoQuery({topicId});
             const topicIdInfo = await topicIdQuery.execute(client).catch(e => {
                 // console.log(e.message)
                 if(e.message.includes(invalidTopicIdMessage)){
@@ -37,11 +37,10 @@ async function checkTopicId() {
 
             if(topicIdInfo) console.log(`Existing topicId: ${topicIdInfo.topicId.toString()}`);
             
-            // Create topic id if not exist using HCS SDK
-            else await updateTopicId();
+            else console.log(`Invalid topic id - ${topicId}. Please comment out this old topic id in your .env.local file to allow us to update it!`);
 
         } else {
-            console.log(`Invalid topic id. Creating new topic id..`);
+            console.log(`Topic id is not defined. Creating new topic id..`);
             await updateTopicId();
         }
     }
@@ -55,22 +54,25 @@ async function checkTopicId() {
 
 const updateTopicId = async () => {
     if (!fs.existsSync(envFilePath)) throw new Error(`${envFilePath} does not exist. Make sure ${envFilePath} is created at root directory.`);
-
-    const topicId = await createTopicId();
-
-    if(!topicId) throw new Error(`Failed to create topic id. Operation abort. Please retry.`);
     
     // Generate new topic Id and append to env variable
     const envFileContents = fs.readFileSync(envFilePath, 'utf8');
     // Create regex to cater for commented env variable [eg: # HEDERA_TOPIC_ID=]
-    const topicIdEnvVariableRegex : RegExp = new RegExp(`\\s*#\\s*${topicIdEnvVariable}=\\w.*\\s*`,'g');
-    
-    // Overwrite existing topic id if exist
-    if(envFileContents.includes(`${topicIdEnvVariable}=`)) fs.writeFileSync(envFilePath, envFileContents.replace(topicIdEnvVariableRegex, `\n${topicIdEnvVariable}=${topicId}`));
-    // Append new topic id if not exist
-    else fs.appendFileSync(envFilePath, `\n${topicIdEnvVariable}=${topicId}`);
+    const topicIdEnvVariableRegex : RegExp = new RegExp(`(\\s*#\\s*)(${topicIdEnvVariable}=)(\\w.*\\s*)`,'g');
 
-    console.log(`Updated topic id: ${topicId}`);
+    // Check on number of occurence 
+    const isEnvVariablematch = envFileContents.match(topicIdEnvVariableRegex);
+    if(isEnvVariablematch && isEnvVariablematch.length > 1) throw new Error(`More than 1 '${topicIdEnvVariable}' is defined in your .env.local file. Please reduce to 1!`);
+    
+    const createdTopicId = await createTopicId();
+    if(!createdTopicId) throw new Error(`Failed to create topic id. Operation abort. Please retry.`);
+
+    // Overwrite existing topic id if exist
+    if(envFileContents.includes(`${topicIdEnvVariable}=`)) fs.writeFileSync(envFilePath, envFileContents.replace(topicIdEnvVariableRegex, `\n$2${createdTopicId}`));
+    // Append new topic id if not exist
+    else fs.appendFileSync(envFilePath, `\n${topicIdEnvVariable}=${createdTopicId}`);
+
+    console.log(`Updated topic id: ${createdTopicId}`);
 }
 
 const createTopicId = async () =>{
